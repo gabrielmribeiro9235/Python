4d1d7d27e9187f9b68179372ff92b5a780b9f3ba
Gabriel <meloribeiro0757@gmail.com>
2025-10-12 23:34:10 -0300

    Alteração no ex1 de POO

--- Básico de POO/1/main1.py
+++ Básico de POO/1/main1.py
@@ -3,7 +3,7 @@ from POO1 import Pessoa
 p1 = Pessoa('Gabriel', 19)
 print(f'{p1.nome} nasceu em {p1.get_ano()}')
 p1.comer('maçã')
-p1.falar('sexo')
+p1.falar('basquete')
 p1.parar_comer()
 p1.falar('futebol')
 p1.comer('morango')
